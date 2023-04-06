c8acd346c302a6e850e27303451b2a50bee19077
Rahul Bose <rbose715@gmail.com>
2023-04-06 20:51:04 +0530

    remove

--- ecommerece_website/settings.py
+++ ecommerece_website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4_pd_y9b)@l)q&1xpkwrr2p5ro%49)hvx1_wj_fft94r@z%wvu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
